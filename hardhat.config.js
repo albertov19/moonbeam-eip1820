5b42de0e6dc194ef79acbaff4c5f4a6a88a409c2
Albertov19 <albertoviera@gmail.com>
2022-01-13 18:25:00 +0100

    added moonbeam

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,44 +8,49 @@ require('@nomiclabs/hardhat-ethers');
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  // Networks
-  networks: {
-    dev: {
-      url: 'http://127.0.0.1:9933',
-      gasPrice: 1000000000,
-      chainId: 1281,
-    },
-    moonbase: {
-      url: 'https://rpc.testnet.moonbeam.network',
-      gasPrice: 1000000000,
-      chainId: 1287,
-    },
-    moonriver: {
-      url: 'https://rpc.moonriver.moonbeam.network',
-      gasPrice: 1000000000,
-      chainId: 1285,
-    },
-  },
-
-  // Solc
-  solidity: {
-    compilers: [
-      {
-        version: '0.5.3',
-        settings: {
-          optimizer: {
-            enabled: false,
-            runs: 200,
-          },
+    // Networks
+    networks: {
+        dev: {
+            url: 'http://127.0.0.1:9933',
+            gasPrice: 1000000000,
+            chainId: 1281,
         },
-      },
-    ],
-  },
+        moonbase: {
+            url: 'https://rpc.testnet.moonbeam.network',
+            gasPrice: 1000000000,
+            chainId: 1287,
+        },
+        moonriver: {
+            url: 'https://rpc.moonriver.moonbeam.network',
+            gasPrice: 1000000000,
+            chainId: 1285,
+        },
+        moonbeam: {
+            url: 'https://rpc.api.moonbeam.network',
+            gasPrice: 100000000000,
+            chainId: 1284,
+        },
+    },
 
-  // Paths
-  paths: {
-    sources: './contracts',
-    cache: './cache',
-    artifacts: './artifacts',
-  },
+    // Solc
+    solidity: {
+        compilers: [
+            {
+                version: '0.5.3',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+        ],
+    },
+
+    // Paths
+    paths: {
+        sources: './contracts',
+        cache: './cache',
+        artifacts: './artifacts',
+    },
 };
